9fc612ce5a8ed742448a602bf2c5ce3356ede8f2
Mukesh Kumar Singh <kumarsinghmukesh237@gmail.com>
2026-02-17 02:06:23 +0530

    some file are added

---
+++ src/Pokemon.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import "./index.css";
+import { PokemonCards } from "./PokemonCards";
+
+export const Pokemon = () => {
+    const [pokemon, setPokemon] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+    const [search,setSearch] = useState("");
+    const API = "https://pokeapi.co/api/v2/pokemon/?limit=650";
+
+    const fetchPokemon = async () => {
+        try {
+            const response = await fetch(API);
+            const data = await response.json();
+
+            const detailedPokemonData = data.results.map(async (curPokemon) => {
+                const res = await fetch(curPokemon.url);
+                const data = await res.json();
+                return data;
+            });
+
+            const detailedResponse = await Promise.all(detailedPokemonData);
+            // console.log(detailedResponse);
+            setPokemon(detailedResponse);
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+            setError(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchPokemon();
+    }, []);
+
+
+    // search functionality
+    const searchData = pokemon.filter((curPokemon) => curPokemon.name.toLowerCase().includes(search.toLowerCase()));
+
+
+    if (loading) {
+        return (
+            <div className="container">
+                <h1>Loading...</h1>
+            </div>
+        );
+    }
+    if (error) {
+        return (
+            <div className="container">
+                <h1>{error.message}</h1>
+            </div>
+        );
+    }
+    return (
+        <section className="container">
+            <header>
+                <h1>Let's Catch Pokémon</h1>
+            </header>
+            <div className="pokemon-search">
+                <input type="text" placeholder="Search Pokemon" value = {search} onChange={(event)=>setSearch(event.target.value)} />
+            </div>
+            <div>
+                <ul className="cards">
+                    {searchData.map((curPokemon) => (
+                        <PokemonCards key={curPokemon.id} pokemonData={curPokemon} />
+                    ))}
+                </ul>
+            </div>
+        </section>
+    );
+};
